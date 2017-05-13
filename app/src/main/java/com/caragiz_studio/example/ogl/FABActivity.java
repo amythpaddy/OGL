1580f22e84e47638847c6b624a465d2298256d0b
amythpaddy <amythpaddy21@gmail.com>
2017-05-13 13:28:09 +0530

    Popup Added

--- app/src/main/java/com/caragiz_studio/example/ogl/FABActivity.java
+++ app/src/main/java/com/caragiz_studio/example/ogl/FABActivity.java
@@ -2,11 +2,16 @@ package com.caragiz_studio.example.ogl;
 
 import android.graphics.Color;
 import android.graphics.drawable.AnimatedVectorDrawable;
+import android.os.Build;
 import android.support.design.widget.FloatingActionButton;
 import android.support.graphics.drawable.AnimatedVectorDrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class FABActivity extends AppCompatActivity {
 
@@ -16,24 +21,46 @@ public class FABActivity extends AppCompatActivity {
         setContentView(R.layout.activity_fab);
     }
     boolean flag = false;
-    public void animate(View view){
-        FloatingActionButton fab = (FloatingActionButton)view;
+    public void animate(View view) {
+        FloatingActionButton fab = (FloatingActionButton) view;
         AnimatedVectorDrawable animVector;
 //        AnimatedVectorDrawable animVector;
-        if(!flag){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            if (!flag) {
 //            animVector = AnimatedVectorDrawableCompat.create(this , R.drawable.tomute_animated_vector_drawable_start_to_end);
-            animVector = (AnimatedVectorDrawable)getDrawable(R.drawable.tomute_animated_vector_drawable_start_to_end);
-            fab.setImageDrawable(animVector);
-            animVector.start();
+                animVector = (AnimatedVectorDrawable) getDrawable(R.drawable.tomute_animated_vector_drawable_start_to_end);
 
-        }
-        else{
-            animVector = (AnimatedVectorDrawable)getDrawable(R.drawable.tomute_animated_vector_drawable_end_to_start);
+                fab.setImageDrawable(animVector);
+
+                animVector.start();
+
+            } else {
+                animVector = (AnimatedVectorDrawable) getDrawable(R.drawable.tomute_animated_vector_drawable_end_to_start);
 //            animVector = AnimatedVectorDrawableCompat.create(this , R.drawable.tomute_animated_vector_drawable_end_to_start);
-            fab.setImageDrawable(animVector);
-            animVector.start();
-        }
+                fab.setImageDrawable(animVector);
+                animVector.start();
+            }
 
-        flag = !flag;
+            flag = !flag;
+        }
+    }
+
+    /***
+     * Displays a popup on click of the view
+     * @param view
+     */
+    public void popup(final View view){
+        PopupMenu pop = new PopupMenu(this , view);
+        pop.getMenuInflater().inflate(R.menu.popup_menu,pop.getMenu());
+        pop.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                Toast.makeText(FABActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+                ((TextView)view).setText(item.getTitle());
+                return true;
+            }
+        });
+
+        pop.show();
     }
 }
